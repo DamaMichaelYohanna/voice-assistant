1e43b7e82ba56c0046a220835ea5b0436f7a6ed3
DamaMichaelYohanna <get2dama11@gmail.com>
2022-09-27 16:44:24 +0100

    updated

--- voice_command.py
+++ voice_command.py
@@ -1,3 +1,13 @@
 import speech_recognition as sr
-for index, name in enumerate(sr.Microphone.list_microphone_names()):
-    print("Microphone with name \"{1}\" found for `Microphone(device_index={0})`".format(index, name))
+import webbrowser
+import pyttsx3 as pyt
+
+engine = pyt.init()
+
+def main():
+    engine.say("hello")
+
+def lunch_browser(url):
+    url = ''
+    webbrowser.open('google.com')
+    
