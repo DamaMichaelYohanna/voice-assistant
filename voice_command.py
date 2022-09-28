73f474b5f1d0d3650c09a0405367ff6dbef36208
DamaMichaelYohanna <get2dama11@gmail.com>
2022-09-28 22:25:18 +0100

    updated

--- voice_command.py
+++ voice_command.py
@@ -3,9 +3,22 @@ import webbrowser
 import pyttsx3 as pyt
 
 engine = pyt.init()
+def open_browser():
+    print("am going to open the browser")
 
 def main():
     engine.say("hello")
+    while True:
+        command = input("Enter your command")
+        if command == "open browser":
+            pass
+        elif command == "play music":
+            pass
+        elif command == "search ofline":
+            pass
+        elif command == "search online":
+            pass
+        pass
 
 def lunch_browser(url):
     url = ''
